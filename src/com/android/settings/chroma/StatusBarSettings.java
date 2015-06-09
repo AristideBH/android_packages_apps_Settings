678f8bdc726e40bce10ed95466c829f0b2b19a73
zephiK <zephik@gmail.com>
2015-06-09 15:52:32 -0400

    Add double tap to sleep on status bar

--- src/com/android/settings/chroma/StatusBarSettings.java
+++ src/com/android/settings/chroma/StatusBarSettings.java
@@ -13,19 +13,28 @@ import com.android.settings.SettingsPreferenceFragment;
 public class StatusBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
-		// private variables here
+    private static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 		
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.chroma_settings_navigation);
+
+       // Status bar double-tap to sleep
+	mStatusBarDoubleTapSleepGesture = (SwitchPreference) getPreferenceScreen().findPreference(DOUBLE_TAP_SLEEP_GESTURE);
+	mStatusBarDoubleTapSleepGesture.setChecked((Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+	Settings.System.DOUBLE_TAP_SLEEP_GESTURE, 0) == 1));
     }
 
-    @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-
-		// preference changes here
+        ContentResolver cr = getActivity().getContentResolver();
+	boolean value = (Boolean) objValue;
+        if (preference == mStatusBarDoubleTapSleepGesture) {
+		Settings.System.putInt(cr,
+		Settings.System.DOUBLE_TAP_SLEEP_GESTURE, value ? 1: 0);
+		return true;
+	}
         return false;
     }
 }
