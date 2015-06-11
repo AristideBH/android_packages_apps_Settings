SimpleAOSP-Lollipop/platform_packages_apps_Settings

Rotation tile: Longclick action [2/2]

src/com/android/settings/Settings.java
@@ -18,6 +18,7 @@ package com.android.settings;
 
 import com.android.settings.applications.AppOpsSummary;
 import com.android.settings.blacklist.BlacklistSettings;
+import com.android.settings.simpleaosp.DisplayRotation;
 
 /**
  * Top-level Settings activity
@@ -103,5 +104,6 @@ public class Settings extends SettingsActivity {
     public static class TopLevelSettings extends SettingsActivity { /* empty */ }
     public static class ApnSettingsActivity extends SettingsActivity { /* empty */ }
     public static class BlacklistSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class DisplayRotationSettingsActivity extends SettingsActivity { /* empty */ }
 }
 

src/com/android/settings/SettingsActivity.java
@@ -113,6 +113,7 @@ import com.android.settings.wifi.AdvancedWifiSettings;
 import com.android.settings.wifi.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
+import com.android.settings.simpleaosp.DisplayRotation;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -299,7 +300,8 @@ public class SettingsActivity extends Activity
             OtherSoundSettings.class.getName(),
             QuickLaunchSettings.class.getName(),
             ApnSettings.class.getName(),
-            BlacklistSettings.class.getName()
+            BlacklistSettings.class.getName(),
+	    DisplayRotation.class.getName()
     };
 
 

AristideBH/android_packages_apps_Settings

src/com/android/settings/Settings.java
@@ -16,7 +16,12 @@
 
 package com.android.settings;
 
+<<<<<<< HEAD
 import com.android.settings.applications.AppOpsSummary;
+=======
+import com.android.settings.applications.AppOpsCategory;
+import com.android.settings.chroma.DisplayRotation;
+>>>>>>> 003d68c... Rotation tile: Longclick action [2/2]
 
 /**
  * Top-level Settings activity
@@ -101,5 +106,6 @@ public class Settings extends SettingsActivity {
 
     public static class TopLevelSettings extends SettingsActivity { /* empty */ }
     public static class ApnSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class DisplayRotationSettingsActivity extends SettingsActivity { /* empty */ }
 }
 

src/com/android/settings/SettingsActivity.java
@@ -112,6 +112,11 @@ import com.android.settings.wifi.AdvancedWifiSettings;
 import com.android.settings.wifi.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
+<<<<<<< HEAD
+=======
+import com.android.settings.headsup.HeadsUpSettings;
+import com.android.settings.chroma.DisplayRotation;
+>>>>>>> 003d68c... Rotation tile: Longclick action [2/2]
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -302,7 +307,8 @@ public class SettingsActivity extends Activity
             AppNotificationSettings.class.getName(),
             OtherSoundSettings.class.getName(),
             QuickLaunchSettings.class.getName(),
-            ApnSettings.class.getName()
+            ApnSettings.class.getName(),
+    	    DisplayRotation.class.getName()
     };
 
 